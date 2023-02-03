e62ef882462f0f4898c9a49100cfe0917c839edb
Dominika N <dominikaniem@gmail.com>
2023-02-03 22:43:06 +0100

    homewok5

---
+++ cypress/e2e/3-tests_dominika/homeworkM5.cy.js
@@ -0,0 +1,11 @@
+describe("take screenshot of cdp-course", () => {
+   
+   it('enters main page' , () => {
+cy.visit('https://fabrykatestow.pl/')
+   })
+    
+it('open section and find cdp' , () => {
+    })
+
+    
+}) 
