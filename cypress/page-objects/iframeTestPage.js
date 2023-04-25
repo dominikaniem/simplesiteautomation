1c4de84b97343b22f1f47167b01169ce68c230d3
Dominika N <dominikaniem@gmail.com>
2023-04-25 22:24:08 +0200

    status code fixes

--- cypress/page-objects/iframeTestPage.js
+++ cypress/page-objects/iframeTestPage.js
@@ -1,34 +1,7 @@
-// const iframe = 'iframe'
-// const button1 = "#simpleButton1"
-// const button2 = "#simpleButton2"
-// const message = '#whichButtonIsClickedMessage'
-// const getIframeBody = () =>  {
-//      return cy.get(iframe)
-//     .its('0.contentDocument.body')
-//         .should('be.visible')
-//         .then(cy.wrap) 
-// ;}
-
-
-// class IframeTestPage {
-    
-//     testButton1() {
-        
-//     getIframeBody().find(button1).click()
-//     getIframeBody().find(message).should('have.text', 'Button 1 was clicked!')
-//     }
-
-
-//     testButton2() {
-        
-//     getIframeBody().find(button2).click()
-//     getIframeBody().find(message).should('have.text', 'Button 2 was clicked!')
-//         }
-// }
-// export default IframeTestPage;
-
-
 // o co cho z return - Aby zwrócić wartość z funkcji, musimy użyć słowa return i dzięki temu zostanie ona zwrócona z wywołania tej funkcji
+// potrzebna zmienna dla iframe
+// wrap this element so we can use cypress commands on it
+
 
 const iFrame = 'iframe'
 const button1 = '#simpleButton1'
--- cypress/page-objects/statusCodePage.js
+++ cypress/page-objects/statusCodePage.js
@@ -1,90 +1,39 @@
-const code200 = '#200siteAnchor'
-const code305 = '#305siteAnchor'
-const code404 = '#404siteAnchor'
-const code500 = '#500siteAnchor'
+const code200 = 'http://httpstat.us/200'
+const code305 = 'http://httpstat.us/305'
+const code404 = 'http://httpstat.us/404'
+const code500 = 'http://httpstat.us/500'
 
-class StatusCodePage {
-    testCode200 () {
-        cy.get(code200).invoke('attr', 'href').then(href => {
-        cy.request(href).then((response) => {
-        expect(response.status).to.eq(200); //https://stackoverflow.com/questions/48348354/cypress-get-href-attribute
-         })
-     })
+
+class StatusCodePage{
+
+testCode200(){
+        cy.request(code200).then((response) => {
+        const body = JSON.stringify(response.body)
+        expect(response.body).to.eq('200 OK')
+        })
     }
 
-    testCode305 () {
-        cy.get(code305).invoke('attr', 'href').then((href) => {
-        cy.request({
-        url: href,
-        followRedirect: false, 
-        }).then((response) => {
-        expect(response.status).to.eq(305);
-        });
-    });
+   testCode305(){
+        cy.request({url: code305, followRedirect: false}).then((response) => {
+        const body = JSON.stringify(response.body)            
+        expect(response.body).to.eq('305 Use Proxy')
+        })
+    }
+
+
+testCode404(){
+        cy.request({url: code404, failOnStatusCode: false}).then((response) => {
+        const body = JSON.stringify(response.body)
+        expect(response.body).to.eq('404 Not Found')
+        })
+    }
+
+    testCode500(){
+        cy.request({url: code500, failOnStatusCode: false} ).then((response) => { 
+        const body = JSON.stringify(response.body)
+        expect(response.body).to.eq('500 Internal Server Error')
+        })
+    }
+
 }
-    
-    testCode404 () {
-        cy.get(code404).invoke('attr', 'href').then((href) =>{ 
-        cy.request({
-        url: href,                
-        failOnStatusCode: false
-        }).then((response) => {
-        expect(response.status).to.eq(404);
-            });
-        });
-    }
-    testCode500 () {
-        cy.get(code500).invoke('attr', 'href').then((href) =>{ 
-        cy.request({
-        url: href,                
-        failOnStatusCode: false
-        }).then((response) => {
-        expect(response.status).to.eq(500);
-            });
-        });
-    }
-     
-}
-
-
- export default StatusCodePage;
-
- //const url200 = 'http://httpstat.us/200'
-// const url305 = 'http://httpstat.us/305'
-// const url404 = 'http://httpstat.us/404'
-// const url500 = 'http://httpstat.us/500'
-
-
-// class StatusCodesPage{
-
-//     checkStatusCode200(){
-//         cy.request(url200).then((response) => {
-//         const body = JSON.stringify(response.body)
-//         expect(response.body).to.eq('200 OK')
-//         })
-//     }
-
-//     checkStatusCode305(){
-//         cy.request({url: url305, followRedirect: false}).then((response) => {
-//         const body = JSON.stringify(response.body)            
-//         expect(response.body).to.eq('305 Use Proxy')
-//         })
-//     }
-
-
-//     checkStatusCode404(){
-//         cy.request({url: url404, failOnStatusCode: false}).then((response) => {
-//         const body = JSON.stringify(response.body)
-//         expect(response.status).to.eq(404)
-//         })
-//     }
-
-//     checkStatusCode500(){
-//         cy.request({url: url500, failOnStatusCode: false} ).then((response) => { 
-//         const body = JSON.stringify(response.body)
-//         expect(response.body).to.eq('500 Internal Server Error')
-//         })
-//     }
-
-// }
-// export default StatusCodesPage;
+export default StatusCodePage;
